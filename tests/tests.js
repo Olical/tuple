b5af75ccb2a41640a9c9ea42d34f6dcb8b7106a2
Oliver Caldwell <olliec87@gmail.com>
2013-07-29 21:22:15 +0100

    Added the base class and tests.

--- tests/tests.js
+++ tests/tests.js
@@ -9,6 +9,57 @@
 		test('the class is defined within the global scope', function () {
 			assert.isFunction(Tuple);
 		});
+
+		test('can create an instance of the class without arguments', function () {
+			var t = new Tuple();
+			assert.instanceOf(t, Tuple);
+		});
+
+		test('can create an instance of the class with arguments', function () {
+			var t = new Tuple('foo', 'bar', 'baz');
+			assert.instanceOf(t, Tuple);
+		});
+	});
+
+	suite('Tuple#unpack', function () {
+		test('can unpack an empty tuple', function () {
+			var t = new Tuple();
+			var args;
+			t.unpack(function () {
+				args = arguments;
+			});
+			assert.deepEqual(args, []);
+		});
+
+		test('can unpack one value', function () {
+			var value = 'foo';
+			var t = new Tuple(value);
+			var args;
+			t.unpack(function () {
+				args = arguments;
+			});
+			assert.deepEqual(args, [value]);
+		});
+
+		test('can unpack multiple values', function () {
+			var first = 'foo';
+			var second = 'bar';
+			var t = new Tuple(first, second);
+			var args;
+			t.unpack(function () {
+				args = arguments;
+			});
+			assert.deepEqual(args, [first, second]);
+		});
+
+		test('returns the returned value from the unpacker', function () {
+			var value = 'foo';
+			var t = new Tuple();
+			var result = t.unpack(function () {
+				return value;
+			});
+			assert.strictEqual(result, value);
+		});
 	});
 
 	mocha.run();
--- tuple.js
+++ tuple.js
@@ -9,10 +9,29 @@
 	'use strict';
 
 	/**
+	 * Simple tuple implementation. This constructor will create new instances
+	 * and store immutable values within them.
+	 *
+	 * @class Tuple A tiny tuple implementation.
+	 * @param {...*} List of values to store within the tuple.
 	 */
 	function Tuple() {
+		this._values = Array.prototype.slice.call(arguments, 0);
 	}
 
+	/**
+	 * Passes the values as arguments, in the same order they were set, to the
+	 * provided unpacker function. It will return the value that the unpacker
+	 * returns.
+	 *
+	 * @param {Function} unpacker Is passed all of the tuples values in order, it's return value will be returned.
+	 * @return {*} The value that the unpacker function returns.
+	 */
+	Tuple.prototype.unpack = function unpack(unpacker) {
+		return unpacker.apply(null, this._values);
+	};
+
+	// Expose the class via AMD, CommonJS or the global object.
 	if (typeof define === 'function' && define.amd) {
 		define(function () {
 			return Tuple;
